c58718fc930702d882b464c773f34ed23d3f6937
wpszz <shenzizai@qq.com>
2018-11-30 10:01:59 +0800

    add Camera-Object Space matrix which is contrasted to the Camera Space matrix

--- RenderCore/Camera.cs
+++ RenderCore/Camera.cs
@@ -23,10 +23,40 @@ namespace WPRenderer
             this.farClipPlane = farClipPlane;
         }
 
+        // just compare with CameraToWorldMatrix(like Unity Transform.LocalToWorldMatrix)
+        public Matrix4x4 LocalToWorldMatrix()
+        {
+            // TR = {0000, 0000, 0000, xyz1} * {right, up, forward, 0001}
+            Vector3 forward = (lookAt - pos).normalized;    // positive Z-Axis in the world
+            Vector3 right = Vector3.Cross(this.up, forward).normalized;
+            Vector3 up = Vector3.Cross(forward, right);
+            return new Matrix4x4(
+                right.x, up.x, forward.x, pos.x,
+                right.y, up.y, forward.y, pos.y,
+                right.z, up.z, forward.z, pos.z,
+                0, 0, 0, 1
+            );
+        }
+
+        // just compare with WorldToCameraMatrix(like Unity Transform.WorldToCameraMatrix)
+        public Matrix4x4 WorldToLocalMatrix()
+        {
+            // (TR)^-1 = (R^-1) * (T^-1)
+            Vector3 forward = (lookAt - pos).normalized; // positive Z-Axis in the world
+            Vector3 right = Vector3.Cross(this.up, forward).normalized;
+            Vector3 up = Vector3.Cross(forward, right);
+            return new Matrix4x4(
+                right.x,    right.y,    right.z,    -Vector3.Dot(right, pos),
+                up.x,       up.y,       up.z,       -Vector3.Dot(up, pos),
+                forward.x,  forward.y,  forward.z,  -Vector3.Dot(forward, pos),
+                0,          0,          0,          1
+            );
+        }
+
         public Matrix4x4 CameraToWorldMatrix()
         {
             // TR = {0000, 0000, 0000, xyz1} * {right, up, forward, 0001}
-            Vector3 forward = (pos - lookAt).normalized;
+            Vector3 forward = (pos - lookAt).normalized;    // negative Z-Axis in the world
             Vector3 right = Vector3.Cross(forward, this.up).normalized;
             Vector3 up = Vector3.Cross(right, forward);
             return new Matrix4x4(
@@ -40,7 +70,7 @@ namespace WPRenderer
         public Matrix4x4 WorldToCameraMatrix()
         {
             // (TR)^-1 = (R^-1) * (T^-1)
-            Vector3 forward = (pos - lookAt).normalized;
+            Vector3 forward = (pos - lookAt).normalized;    // negative Z-Axis in the world
             Vector3 right = Vector3.Cross(forward, this.up).normalized;
             Vector3 up = Vector3.Cross(right, forward);
             return new Matrix4x4(
